326c4ecd89f3a796e8f54089cc81c8cc4ce466b3
Mariekoehle <64197124+Mariekoehle@users.noreply.github.com>
2021-06-06 17:10:39 +0200

    add loginform und registerform

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,22 @@
 <template>
-  <div>
-  </div>
+<div>
+  <Loginform/>
+  <Registerform/>
+</div>
 </template>
 
 <script>
+import Loginform from './components/Loginform.vue'
+import Registerform from './components/Registerform.vue'
+
 export default {
+  name: 'App',
+  components: {
+    Registerform,
+    Loginform
+  }
 }
 </script>
 
 <style>
-
 </style>
---
+++ frontend/src/components/Loginform.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="login-form">
+     <h3>register</h3>
+    <form
+    @submit.prevent="loginUser"
+    metod="post"
+    novalidate
+    autocomplete="off"
+    >
+        <label for="username">user name:</label><br>
+        <input type="text" id="username" name="username" value="" v-model="username"><br>
+        <label for="password">password:</label><br>
+        <input type="text" id="password" name="password" value="" v-model="password"><br>
+        <input type="submit" value="register">
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Loginform',
+  props: {},
+  data () {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    loginUser: function () {
+      const userData = {
+        username: this.username,
+        password: this.password
+      }
+      const uri = 'http://localhost:3001/api/login'
+      axios.post(uri, userData)
+        .then(function (response) {
+          console.log(response)
+        })
+        .catch(function (err) {
+          console.log(err)
+        })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
