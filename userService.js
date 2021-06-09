da6b10a5d1941b8c3265a62521c216af7abda596
Mariekoehle <64197124+Mariekoehle@users.noreply.github.com>
2021-06-09 20:15:14 +0200

    users
    hashed password

--- userService.js
+++ userService.js
@@ -1,145 +1,94 @@
-const {v4: uuidv4} = require('uuid');
-let db, users;
-let sessions = {};
-
+const { v4: uuidv4 } = require('uuid');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 //MONGO DB
 const MongoClient = require('mongodb').MongoClient;
-const dbaccess = require('./config').config;
-const uri = `mongodb+srv://${dbaccess.user}:${dbaccess.password}@${dbaccess.host}/${dbaccess.name}?retryWrites=true&w=majority`;
-const dbClient = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+const uri = "mongodb+srv://marie:Rdm0LsnFudSUPuA7@cluster0.32yao.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const dbClient = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+var db;
+var userData;
+var sessions = {};
 dbClient.connect(err => {
-  if (err) {
-    console.log(err);
-  }
-  else {
-    console.log("connection to database established");
+    console.log("Connecting to User Service ...");
     db = dbClient.db("moistureTracking");
-
-    db.createCollection("users", function (err, res) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("created collection 'users'.");
-      }
-    });
-    users = db.collection("users");
-  }
+    userData = db.collection("users");
 });
-
-//EXPRESS
+//EXPRESS:
 const express = require('express');
 const app = express();
-const cors = require('cors');
-const corsOptions = {
-  cors: {
-    origin: "http://localhost:8080",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  }
-};
-app.use(cors(corsOptions));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
-const port = 3002;
-
+app.use(express.urlencoded({ extended: true }));
+const cors = require('cors');
+const corsSettings = {
+    cors: {
+        origin: "http://localhost:8080",
+        methods: ["GET", "POST", "PUT", "DELETE"]
+    }
+};
+app.use(cors(corsSettings));
+const port = 3001;
 app.listen(port, () => {
-  console.log(`user service live @ http://localhost:${port}`);
+    console.log(`user data service live @ http://localhost:${port}`);
 });
-
 app.post('/api/register', (req, res) => {
-  if (users) {
+    console.log("registering new user");
     let user = req.body;
-    user.createdAt = new Date();
     console.log(user);
-    users.insertOne(user, (dbErr, dbRes) => {
-      if (dbErr) {
-        console.log(dbErr);
-      } else {
-        //console.log("created new user " + user.username);
-        res.send({
-          action: "register",
-          result: "success."
-        });
-      }
-    });
-  } else {
-    res.send({
-      action: "register",
-      result: "user database missing."
-    });
-  }
-});
-
-app.post('/api/login', (req, res) => {
-  console.log("Login try:");
-  console.log(req.body);
-  if (users) {
-    users.find({username: req.body.username}).next()
-      .then((dbres) => {
-        if (dbres) {
-          console.log("found username in database.");
-          if (dbres.password === req.body.password) {
-            const session = createSession(dbres._id);
-            sessions[session.token] = session;
-            console.log("\nCurrently active sessions:");
-            console.log(sessions);
-            res.send({
-              action: "login",
-              result: "success",
-              token: session.token,
+    user.createdAt = new Date();
+    user.hashedPass = bcrypt.hashSync(user.password, saltRounds);
+    delete user.password;
+    console.log(user);
+    userData.insertOne(user, (err, dbres) => {
+        // console.log("showing user's password to the world");
+        if (err) {
+            console.log("error");
+            res.status(500).send({
+                "action": "register",
+                "success": false,
+                "message": "error while inserting into mongoDB",
+                "error": err
             });
-          } else {
-            res.send({
-              action: "login",
-              result: "wrong password",
-              token: null,
-            });
-          }
-        } else {
-          console.log("couldn't find user in database.");
-          res.send({
-            action: "login",
-            result: "unknown user",
-            token: null,
-          });
         }
-      });
-  }
-});
-
-app.post('/api/logout', (req, res) => {
-  const token = req.body.token;
-  if (checkToken) {
-    delete session[token];
-    res.send({
-      action: "logout",
-      result: "success"
-    });
-  } else {
-    res.send({
-      action: "logout",
-      result: "invalid token"
-    });
-  }
-});
-
-app.post('/api/validateToken', (req, res) => {
-  let token = req.body.token;
-  res.send({
-    action: "validateToken",
-    result: checkToken(token) ? "success" : "invalid token",
-  });
-});
-
-function createSession(userId) {
-  return {
-    createdAt: new Date(),
-    lastAction: new Date(),
-    userId: userId,
-    token: uuidv4()
-  };
-}
-
-function checkToken(token) {
-  return sessions.hasOwnProperty(token);
+        else {
+            console.log("created new user");
+            res.status(200).send({
+                "action": "register",
+                "success": true,
+                "message": "created new user",
+                "error": null
+            });
+        }
+    })
+})
+app.post('/api/login', (req, res) => {
+    console.log("logging in user");
+    let sentdata = req.body;
+    userData.findOne({ username: sentdata.username })
+        .then((dbres) => {
+            console.log("found user");
+            if (bcrypt.compareSync(sentdata.password, dbres.hashedPass)) {
+                console.log("login successful");
+                let token = uuidv4();
+                createSession(dbres, token);
+                // console.log(sessions);
+                res.status(200).send({
+                    action: "login",
+                    success: true,
+                    message: "logged in",
+                    error: null,
+                    token: token
+                });
+            }
+            else {
+                res.status(500);
+            }
+        })
+})
+function createSession(userData, token) {
+    sessions[token] = {
+        userId: userData._id,
+        createdAt: new Date(),
+        modifiedAt: new Date(),
+        token: token
+    }
 }
